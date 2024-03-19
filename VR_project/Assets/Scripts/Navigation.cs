3faa118a3da600b205e6c5ab0c4c5ff500df1fb8
John Vlamis <123981368+johnvl02@users.noreply.github.com>
2024-03-19 23:24:50 +0200

    add some npcs

---
+++ VR_project/Assets/Scripts/Navigation.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentPatrol : MonoBehaviour
+{
+    public Transform[] targets;
+    int targetIndex;
+    NavMeshAgent agent;
+    public Animator animator;
+    float timer;
+    float waitTime;
+    public float waitTimeMin = 6f;
+    public float waitTimeMax = 7f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        timer = 0;
+
+        targetIndex = -1;
+        GoNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < agent.stoppingDistance && !agent.pathPending)
+        {
+            animator.SetBool("Walking", false);
+
+            timer = timer + Time.deltaTime;
+
+            if (timer >= waitTime)
+            {
+                GoNext();
+            }
+        }
+
+    }
+
+    void GoNext()
+    {
+        timer = 0;
+
+        waitTime = Random.Range(waitTimeMin, waitTimeMax);
+
+        //i
+        targetIndex = (targetIndex + 1) % targets.Length;
+
+        //animation
+        animator.SetBool("Walking", true);
+
+        agent.SetDestination(targets[targetIndex].position);
+    }
+}
